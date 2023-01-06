5be9dee1b135f7c2886befe098c720cff1f384a3
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-01-06 11:44:07 +0100

    parameterize getMappingTable

--- R/01-getMappingTable.R
+++ R/01-getMappingTable.R
@@ -1,8 +1,9 @@
 #' getMappingTable
 #'
+#' @param mappingName file name of the mapping
 #' @export
-getMappingTable <- function() {
-  mappingfile <- system.file("mapping", "Field_Mapping.csv", package = "MpiIsoData")
+getMappingTable <- function(mappingName = "Field_Mapping.csv") {
+  mappingfile <- system.file("mapping", mappingName, package = "MpiIsoData")
   mapping <- read.csv2(file = mappingfile, stringsAsFactors = FALSE, check.names = FALSE,
                        na.strings = c("", "NA"), strip.white = TRUE)
   mapping
