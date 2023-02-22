2e16fad9387faf8e9c38f7695094825fab4e1a4c
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-02-22 21:18:53 +0100

    fix test

--- tests/testthat/test-load.R
+++ tests/testthat/test-load.R
@@ -1,8 +1,8 @@
-test_that("deleteOldQry", {
+test_that("deleteOldDataQry", {
   for (mapping in mappingNames()) {
     for (db in dbnames()) {
       expect_equal(
-        deleteOldQry(mappingName = mapping, table = "data", source = db) %>% as.character(),
+        deleteOldDataQry(mappingName = mapping, table = "data", source = db) %>% as.character(),
         paste0("DELETE FROM `", paste0(c(mapping, "data"), collapse = "_"), "` WHERE `source` = '", db, "';")
         )
     }
