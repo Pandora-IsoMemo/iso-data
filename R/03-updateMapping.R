4708519d42aaf70b7df2b9e563d24b8d1dc26613
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-02-27 21:01:34 +0100

    shift updateOrCreateTbale, fix test

--- R/03-updateMapping.R
+++ R/03-updateMapping.R
@@ -23,31 +23,3 @@ etlMapping <- function(){
 
   ## sendData(mappingSource, table = "mappingSource", mode = "truncate")
 }
-
-#' Update Or Create Table MPI
-#'
-#' @param dat (data.frame) data to write
-#' @param prefix (character) prefix of table, e.g. mappingName
-#' @param table (character) name of table on the server without prefix, e.g. "mapping"
-#' @param mode (character) mode
-updateOrCreateTableMPI <- function(dat, prefix, table, mode) {
-  browser()
-  if (prefix == "Field_Mapping") {
-    # update the old table without prefix (here a prefix was not used yet)
-    sendDataMPI(dat, table = table, mode = mode)
-  } else {
-    # check if table exists
-    table <- paste0(c(prefix, table), collapse = "_")
-    tableExists <- sendQueryMPI(dbtools::Query(tableExistsQry(), table = table))
-    if (tableExists == 1) {
-      sendDataMPI(dat, table = table, mode = mode)
-    } else {
-      # if not create one -> which column specs are required?
-      # create table query ...
-    }
-  }
-}
-
-tableExistsQry <- function() {
-  "IF OBJECT_ID ('{{ table }}', 'U') IS NOT NULL SELECT 1 AS res ELSE SELECT 0 AS res;"
-}
--- tests/testthat/test-load.R
+++ tests/testthat/test-load.R
@@ -2,7 +2,7 @@ test_that("deleteOldDataQry", {
   testMapping <- "Field_Mapping"
   for (db in dbnames()) {
     expect_equal(
-      deleteOldDataQry(mappingName = testMapping, table = "data", source = db) %>% as.character(),
+      deleteOldDataQry(testMapping, table = "data", source = db) %>% as.character(),
       paste0("DELETE FROM `data` WHERE `source` = '", db, "';")
     )
   }
