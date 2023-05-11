70de074392baec80c60bc45d3f8840c002187511
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-05-11 20:07:19 +0200

    fix wrong package name

--- R/01-send.R
+++ R/01-send.R
@@ -11,7 +11,7 @@ getQuery <- function(query) {
   if (grepl(";$", query)) query
   else file(system.file(
     sprintf("sql/%s.sql", sub("\\.sql$", "", query, ignore.case = TRUE)),
-    package = "MpiIsoApi",
+    package = "MpiIsoData",
     mustWork = TRUE
   ))
 }
