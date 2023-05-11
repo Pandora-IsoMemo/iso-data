49baa0e987d21d917a926cf2b7564d03f73c7155
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-05-11 20:37:28 +0200

    fix queries

--- inst/sql/mappingId_data.sql
+++ inst/sql/mappingId_data.sql
@@ -1,5 +1,3 @@
-IF (EXISTS (SELECT *
-              FROM INFORMATION_SCHEMA.TABLES
-            WHERE TABLE_NAME = `{{ tableName }}`))
-DELETE FROM `{{ tableName }}` WHERE `source` = `{{ dbSource }}` ELSE
 CREATE TABLE IF NOT EXISTS `{{ tableName }}` ({{ colDefs }}, PRIMARY KEY (`source`,`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DELETE FROM `{{ tableName }}` WHERE `source` = `{{ dbSource }}`;
