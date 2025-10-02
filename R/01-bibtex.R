661d8e63fda64a7a7cda58c36a1086d8c8f46420
Antonia Runge <antonia.runge@inwt-statistics.de>
2025-10-02 09:19:32 +0200

    add logging, launch.json and .lintr

--- R/01-bibtex.R
+++ R/01-bibtex.R
@@ -7,32 +7,45 @@ add_bibtex_column <- function(
   citationformat = "bibtex",
   citationstyle = "apa"
 ) {
-  logging("Adding BibTeX citations for %s", doi_column)
   unique_doi_url <- unique(df[[doi_column]])
+  unique_doi_url <- unique_doi_url[!is.na(unique_doi_url) & unique_doi_url != ""]
+
+  log_str <- sprintf(
+    "Adding BibTeX citations for %s. Found %i unique DOI%s ",
+    doi_column,
+    length(unique_doi_url),
+    ifelse(length(unique_doi_url) != 1, "s", "")
+  )
 
   # split "http://dx.doi.org/" or "https://doi.org/" prefix if present from DOI
   prefix <- sub("^(?i)(https?://(?:dx\\.)?doi\\.org/).*", "\\1", unique_doi_url, perl = TRUE)
   dois   <- sub("^(?i)https?://(?:dx\\.)?doi\\.org/", "", unique_doi_url, perl = TRUE)
 
   df[[citation_column]] <- NA_character_
-  lapply(seq_along(unique_doi_url), function(i) {
+  for (i in seq_along(unique_doi_url)) {
     doi <- dois[i]
-    if (is.na(doi) || doi == "") {
-      return(NULL)
-    }
     citation <- tryCatch({
       rcrossref::cr_cn(doi = doi, format = citationformat, style = citationstyle)
     }, error = function(e) {
       warning(paste("Failed to retrieve citation for DOI:", doi))
-      return(NULL)
+      log_str <- paste0(log_str, "f")
+      NULL
     })
 
     if (!is.null(citation)) {
       index <- !is.na(df[[doi_column]]) & df[[doi_column]] == paste0(prefix[i], doi)
-      df[index, citation_column] <<- citation
+      if (any(index)) {
+        df[index, citation_column] <- rep(citation, sum(index))
+        log_str <- paste0(log_str, ".")
+      } else {
+        log_str <- paste0(log_str, "e")
+      }
+    } else {
+      log_str <- paste0(log_str, "n")
     }
-  })
+  }
 
+  logging(log_str)
   return(df)
 }
 
